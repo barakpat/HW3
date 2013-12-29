fd00c9d4a33f3bd49d1f9519c2367e9ca72c5f58
Boris Sirota <borisirota@gmail.com>
2013-12-29 02:22:53 +0200

    added chosen leader delegate

--- HW3_Zookeeper/Program.cs
+++ HW3_Zookeeper/Program.cs
@@ -9,15 +9,21 @@ namespace HW3_Zookeeper
     {
         static void Main(string[] args)
         {
+            HW3_Zookeeper.Distributer.LeaderDelegate leaderDelegate = leader;
             HW3_Zookeeper.Distributer.UpdateDataToPhaseDelegate updateDataToPhaseDelegate = updateDataToPhase;
             HW3_Zookeeper.Distributer.DeleteOldDataDelegate deleteOldDataDelegate = deleteOldData;
-            Distributer d = new Distributer(args[0], args[1], args[2], updateDataToPhaseDelegate, deleteOldDataDelegate);
+            Distributer d = new Distributer(args[0], args[1], args[2], leaderDelegate, updateDataToPhaseDelegate, deleteOldDataDelegate);
             d.join();
             Console.ReadKey();
             d.leave();
             Console.ReadKey();
         }
 
+        public static void leader()
+        {
+            Console.WriteLine("leader choosen");
+        }
+        
         public static void updateDataToPhase(int phase)
         {
             Console.WriteLine("new phase: " + phase);
