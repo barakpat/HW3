1b3f16e3d0d9ae922b61a04b29ce458a5948ea31
barak pat <barakpat@gmail.com>
2014-01-05 17:37:53 +0200

    Add loging support

---
+++ HW1c/LogBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1c
+{
+    class LogBehavior : IOperationBehavior
+    {
+
+        public String file;
+
+        public LogBehavior(String file)
+        {
+            this.file = file;
+        }
+
+        public void AddBindingParameters(OperationDescription operationDescription, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+            return;
+        }
+
+        public void ApplyClientBehavior(OperationDescription operationDescription, System.ServiceModel.Dispatcher.ClientOperation clientOperation)
+        {
+            return;
+        }
+
+        public void ApplyDispatchBehavior(OperationDescription operationDescription, System.ServiceModel.Dispatcher.DispatchOperation dispatchOperation)
+        {
+            dispatchOperation.Invoker = new LogInvoker(this.file, dispatchOperation.Invoker);
+        }
+
+        public void Validate(OperationDescription operationDescription)
+        {
+            return;
+        }
+    }
+}
+
+
